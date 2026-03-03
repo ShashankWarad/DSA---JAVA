ff9b45a1c178a093764b622224f31545b301de2d
ShashankWarad <105542248+ShashankWarad@users.noreply.github.com>
2026-03-03 16:13:00 +0530

    Implement mathematical solution for Josephus problem
    Added a mathematical and optimal solution for the Josephus problem.

--- Recurssion/JosephusProblem.java
+++ Recurssion/JosephusProblem.java
@@ -17,3 +17,19 @@ public class Solution
     return jos(arr, n-1, k, start);
    }
 }
+
+// =====================================================================================================
+
+// MATHEMATICAL AND OPTIMAL SOLUTION -> based on mathematical formula -> [josephus(n, k) = (josephus(n-1, k) + k) % n]
+
+//    public class Solution{
+//       public int josephus(int n, int k){
+//          int result = 0;
+
+//          for(int i = 1; i <= n; i++){
+//             result = (result + k) % i;
+//          }
+
+//          return result + 1;
+//       }
+//    }
